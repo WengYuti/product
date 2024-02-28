c76f4536d88eee6e9973515775ff623feb91b6b3
Yuti Weng <miniq0624@gmail.com>
2024-02-28 16:19:20 +0800

    with open

--- product.py
+++ product.py
@@ -13,4 +13,14 @@ print(products)
 
 for p in products:
 	print(p[0], '$', p[1])
-	
+#字串可以做+及乘
+#'abc' + '123' = 'abc123'
+#'abc' * 3 = 'abcabcabc'
+
+#open只是產生檔案，write才是真的寫入
+#逗點在excel檔裡就是格子的區隔
+#\n為換行
+# with功能可以在你執行完後自動幫你close檔案，所以打開檔案需要執行的程式要寫在with裡面
+with open('products.csv', 'w') as f:
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
